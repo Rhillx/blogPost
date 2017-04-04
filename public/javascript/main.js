a51460c9018f1659632b1988914a998fae0f32d5
Richard Hill III <rhillz718@gmail.com>
2017-04-04 14:57:20 -0400

    resumed project in MVS

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -69,91 +69,92 @@
             });
         } // DELETE
 
-        function render(blogItems) {
-            const container = document.querySelector('.js-blogPost');
-            // container.innerHTML = "";
-            for (const blogItem of blogItems) {
-                const section = document.createElement('section');
-                section.innerHTML =
-    `<div class="inner">
-      <a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+
+
+        function renderToSite(blogItems){
+
+          const container = document.querySelector('.js-blogPost')
+          container.innerHTML= "";
+
+          console.log(container);
+
+          for(const blogItem of blogItems){
+            const section = document.querySelector('.js-section');
+            section.innerHTML=
+  `<div class="inner">
+    <a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
       <div class="content">
         <h2 class="major">${blogItem.data.title}</h2>
-        <p>${blogItem.data.body}</p>
+        <p>${blogItem.data.blog}</p>
         <a href="#" class="special">Read more</a>
       </div>
-    </div>`  ;
+  </div>`;
 
-                section.classList.add('wrapper', 'spotlight', 'style1');
+          section.classList.add('wrapper', 'spotlight', 'style1');
+          container.appendChild(section);
 
-                container.appendChild(section);
 
-
-                // document.querySelector('.js-todo-remove').addEventListener('click', (e) => {
-                //     console.log('about to delete LOL')
-                //     const {id} = blogItem;
-                //
-                //     DELETE('/api/todo/' + blogItem.id)
-                //         .then((data) => {
-                //             render(data);
-                //         })
-                //         .catch((e) => {
-                //             alert(e)
-                //         });
-                // });
-                // li.querySelector('.js-todo-check').addEventListener('click', (e) => {
-                //     console.log(blogItem);
-                //     let isDone;
-                //     if (blogItem.data.isDone) {
-                //         isDone = false;
-                //     } else {
-                //         isDone = true;
-                //     }
-                //
-                //     PUT('/api/blog/' + blogItem.id, {
-                //             isDone
-                //         })
-                //         .then((data) => {
-                //             render(data);
-                //         })
-                //         .catch((e) => {
-                //             alert(e)
-                //         })
-                // })
-
-            }
+          }
         }
 
 
 
-    GET('/api/blogs')
-			.then((blogItems) => {
-        render(blogItems);
-    });
-
-
-    const postBttn = document.querySelector('.js-postBtn'); postBttn.addEventListener('click', (e) => {
-        console.log('trying to post');
-				// console.log(blogItems);
-    	const input = document.querySelector('.js-blogtitle');
-      const body = document.querySelector('.js-blogPostArea')
-    	input.setAttribute('disabled', 'disabled');
-      body.setAttribute('disabled', 'disabled');
 
 
 
 
-    	POST('/api/blogs', {
-    		title: input.value,
-        blog: body.value,
-    		when: new Date().getTime() + 9 * 60 * 60 * 1000
-    	}).then((data) => {
-    		input.removeAttribute('disabled');
-        body.removeAttribute('disabled');
-    		input.value = '';
-        body.value = '';
-    		render(data);
-    	});
-    })
+// calling functions
+
+GET('/api/blogs')
+ .then((blogItems)=>{
+  renderToSite(blogItems)
+});
+
+
+
+
+
+
+//click events
+
+       const creatBtn = document.querySelector('.js-postBtn')
+          creatBtn.addEventListener('click',(e)=>{
+          const input= document.querySelector('.js-blogtitle')
+          const body = document.querySelector('.js-blogPostArea')
+
+
+              POST('/api/blogs', {
+                title: input.value,
+                blog: body.value,
+                when: new Date().getTime() + 9 * 60 * 60 * 1000
+              }).then((data) => {
+                input.removeAttribute('disabled');
+                body.removeAttribute('disabled');
+                input.value = '';
+                body.value = '';
+                // window.location.href = '/';
+            })
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
